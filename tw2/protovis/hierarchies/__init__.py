9dadd8eebec36f01cf8d69890e3ac2f64fb98d75
Ralph Bean <rbean@redhat.com>
2012-03-06 10:02:09 -0500

    Changed links to the new repo.

--- tw2/protovis/hierarchies/__init__.py
+++ tw2/protovis/hierarchies/__init__.py
@@ -2,7 +2,7 @@
 A bunch of cool widgets from protovis (under the 'custom' heading in their
 examples page).
 
-Get this source from http://github.com/ralphbean/tw2.protovis.custom
+Get this source from http://github.com/toscawidgets/tw2.protovis.custom
 
 Protovis itself can be found here:  http://vis.stanford.edu/protovis/
 """
